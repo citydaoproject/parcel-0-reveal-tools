e2d2952eb24ef726f91d587afc60d7624f74eac6
Michael D. Norman <michael.norman@denormans.com>
2022-07-02 11:03:06 -0500

    chore: added basic command-line tool

---
+++ src/cmd/commands/cmd.ts
@@ -0,0 +1,32 @@
+import { Command } from 'commander';
+
+const { version } = require('../../../package.json');
+
+export interface ProgramSubCommand {
+  parent?: ProgramSubCommand;
+
+  opts(): ProgramOptions;
+}
+
+export interface ProgramOptions {}
+
+export const extractProgramOptions = (cmd: ProgramSubCommand): ProgramOptions => {
+  if (cmd.parent) {
+    return extractProgramOptions(cmd.parent);
+  }
+
+  return cmd.opts();
+};
+
+export const program = new Command()
+  .name('parcel-0-reveal-tools')
+  .description('Commands to prepare the parcel 0 reveal')
+  .version(version);
+
+export const print = (...args: any[]) => {
+  console.log(...args);
+};
+
+export const printLines = (lines: any[]) => {
+  lines.forEach((line) => console.log(line));
+};
