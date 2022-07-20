60ca8090cdda5a38ab0976c149cea9f726fcea95
Michael D. Norman <michael.norman@denormans.com>
2022-07-20 07:38:17 -0500

    chore [#5]: remove snapshot tool
    - also removed other tools that aren't in this repo

--- src/cmd/actions/plots.ts
+++ src/cmd/actions/plots.ts
@@ -4,31 +4,6 @@ import { assignPlotsToNFTs, buildOwnerAssignmentsWithPlots, NFTPlot } from '../.
 import { convertPlotsFileToPlots, convertPlotsToPlotsFile, PlotsFile } from '../../services/plot/plot';
 import { print } from '../commands/cmd';
 
-export interface CreatePlotsOptions {
-  sizeTolerance: number;
-}
-
-export const createPlots = (
-  numPlots: number,
-  inputFile: string,
-  outputFile: string,
-  { sizeTolerance }: CreatePlotsOptions,
-) => {
-  print(`Creating ${numPlots} plots from '${inputFile}' into '${outputFile}' with tolerance of ${sizeTolerance}...`);
-
-  // todo: create plots
-
-  print('Done.');
-};
-
-export const createPlotImages = (plotsFile: string, imageFile: string, outputDir: string) => {
-  print(`Creating images from plots in '${plotsFile}' and main image '${imageFile}' into files in '${outputDir}'...`);
-
-  // todo: create images
-
-  print('Done.');
-};
-
 export interface AssignPlotsOptions {
   unusedPlotsFile?: string;
   debugDir?: string;
